50a519fe4a2676f6ffac6e17e2db356a0b90c732
iliakg <piryazevilia@gmail.com>
2012-11-28 11:45:26 +0600

    end authorization

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,8 @@ FirstApp::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-
+  config.assets.precompile += ['blueprint/ie.css']
+  config.assets.initialize_on_precompile = false
   # Generate digests for assets URLs
   config.assets.digest = true
 
